6b847e6630f9ce9e2c1ec983fc9a58c2c479581d
Simerpreet Anand <simer.anand@workday.com>
2019-10-07 21:28:55 +0100

    main.py module entry module

---
+++ haversine/main.py
@@ -0,0 +1,6 @@
+from list_customers import ListCustomers
+
+print('Hi, Please find the list of the customers invited:\n')
+
+if __name__ == '__main__':
+    ListCustomers().print_customer_within_range(100.0)
